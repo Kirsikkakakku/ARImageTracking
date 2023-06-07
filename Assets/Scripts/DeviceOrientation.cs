ed3845dc1b8e27ad1d8bb052d3eac98debe4c1bb
Kirsikkakakku <48278250+Kirsikkakakku@users.noreply.github.com>
2023-06-07 12:28:47 +0300

    .

--- Assets/Scripts/DeviceOrientation.cs
+++ Assets/Scripts/DeviceOrientation.cs
@@ -2,13 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class DeviceOrientation : MonoBehaviour
 {
-    [SerializeField] Text orientationText;
+    [SerializeField] Text orientationText, gyroAvailability;
+
+    public static AttitudeSensor current { get; }
+
+    private bool gyroAvailable = false;
+
+    void Start()
+    {
+        Input.gyro.enabled = true;
+        gyroAvailable = SystemInfo.supportsGyroscope;
+        gyroAvailability.text = gyroAvailable.ToString();
+    }
 
     void Update()
     {
+        if (current == null)
+        {
+            orientationText.text = "NULL";
+        }
         orientationText.text = Input.gyro.attitude.ToString();
     }
 }
