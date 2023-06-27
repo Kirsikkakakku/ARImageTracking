08b9a9ccdc1e1d2a13a96cb90dafccdd01a49e09
Kirsikkakakku <asaha020@edu.xamk.fi>
2023-06-27 10:48:05 +0300

    .

--- Assets/Scripts/TestingText.cs
+++ Assets/Scripts/TestingText.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SpatialTracking;
 using UnityEngine.UI;
 
 namespace Imagine.WebAR
@@ -8,17 +9,26 @@ namespace Imagine.WebAR
     public class TestingText : MonoBehaviour
     {
         private ImageTrackerCamera trackerCamera;
+        private TrackedPoseDriver trackedPoseDriver;
 
-        [SerializeField] private Text testText_1;
+        [SerializeField] private Text testText_1, testText_2, testText_3, testText_4;
 
         private void Awake()
         {
             trackerCamera= FindObjectOfType<ImageTrackerCamera>();
+            trackedPoseDriver = FindObjectOfType<TrackedPoseDriver>();
+        }
+
+        private void Start()
+        {
+            testText_2.text = trackedPoseDriver.deviceType.ToString();
         }
 
         private void Update()
         {
             testText_1.text = trackerCamera.transform.position.ToString();
+            testText_3.text = trackedPoseDriver.transform.position.ToString();
+            testText_4.text = trackedPoseDriver.transform.rotation.ToString();
         }
     }
 }
